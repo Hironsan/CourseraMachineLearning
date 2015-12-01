947dc258a0dc99b0b4ab1aac50479fe9532da331
hirokinakayama <h-nakayama@uec.ac.jp>
2015-12-02 06:41:47 +0900

    refactor code

--- ex7/kmeans.py
+++ ex7/kmeans.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-import sys
-
 from numpy import *
-import scipy.misc, scipy.io, scipy.optimize, scipy.cluster.vq
-from matplotlib import pyplot, cm, colors
-from mpl_toolkits.mplot3d import Axes3D
+import scipy.misc
+from matplotlib import pyplot
 
 
-def findClosestCentroids(X, centroids):
+def find_closest_centroids(X, centroids):
     K = shape(centroids)[0]
     m = shape(X)[0]
     idx = zeros((m, 1))
@@ -23,33 +20,18 @@ def findClosestCentroids(X, centroids):
                 lowest = cost
 
         idx[i] = lowest_index
+
     return idx + 1  # add 1, since python's index starts at 0
-
-
-def computeCentroidsLoop(X, idx, K):
-    m, n = shape(X)
-    centroids = zeros((K, n))
-
-    for k in range(1, K+1):
-        counter = 0
-        cum_sum = 0
-        for i in range(0, m):
-            if idx[i] == k:
-                cum_sum += X[i]
-                counter += 1
-        centroids[k-1] = cum_sum / counter
-    return centroids
 
 
 def compute_centroids(X, idx, K):
     m, n = shape(X)
     centroids = zeros((K, n))
-
     data = c_[X, idx]  # append the cluster index to the X
 
     for k in range(1, K+1):
-        temp = data[data[:, n] == k]  # quickly extract X that falls into the cluster
-        count = shape(temp)[0]        # count number of entries for that cluster
+        temp = data[data[:, n] == k]  # クラスタkに割り当てられたデータを取り出す
+        count = shape(temp)[0]        # クラスタkに割り当てられたデータ数
 
         for j in range(n):
             centroids[k-1, j] = sum(temp[:, j]) / count
@@ -63,13 +45,11 @@ def run_kmeans(X, initial_centroids, max_iters, plot=False):
     idx = None
 
     for iteration in range(max_iters):
-        idx = findClosestCentroids(X, centroids)
-        centroids = compute_centroids(X, idx, K)
+        idx = find_closest_centroids(X, centroids)
 
         if plot is True:
             data = c_[X, idx]
 
-            # Extract data that falls in to cluster 1, 2, and 3 respectively, and plot them out
             data_1 = data[data[:, 2] == 1]
             pyplot.plot(data_1[:, 0], data_1[:, 1], 'ro', markersize=5)
 
@@ -83,6 +63,8 @@ def run_kmeans(X, initial_centroids, max_iters, plot=False):
 
             pyplot.show(block=True)
 
+        centroids = compute_centroids(X, idx, K)
+
     return centroids, idx
 
 
@@ -90,58 +72,39 @@ def kmeans_init_centroids(X, K):
     return random.permutation(X)[:K]
 
 
-def part1_2():
-    mat = scipy.io.loadmat( "./ex7data2.mat" )
-    X 	= mat['X']
-    K 	= 3
-
-    max_iters = 10
-    centroids = array([[3, 3], [6, 2], [8, 5]])
-
-    run_kmeans( X, centroids, max_iters, plot=True )
-
-
 def part1_4():
-    #A = scipy.misc.imread( "./bird_small.png" )
-    #A = scipy.ndimage.imread("./bird_small.png")
-    A = pyplot.imread("./bird_small.png")
+    A = scipy.misc.imread("./bird_small.png")  # (128, 128, 3) dimension
 
-    # shows the image
-    axes 	= pyplot.gca()
-    figure 	= pyplot.gcf()
-    axes.imshow( A )
-    pyplot.show( block=True )
+    pyplot.imshow(A)
+    pyplot.show(block=True)
 
     A = A / 255.0
-    img_size = shape( A )
+    img_size = shape(A)
 
-    X = A.reshape( img_size[0] * img_size[1], 3 )
+    X = A.reshape(img_size[0] * img_size[1], 3)
     K = 16
     max_iters = 10
 
-    initial_centroids = kmeans_init_centroids( X, K )
-    centroids, idx = run_kmeans( X, initial_centroids, max_iters )
+    initial_centroids = kmeans_init_centroids(X, K)
+    centroids, idx = run_kmeans(X, initial_centroids, max_iters)
 
     # mapping the centroids back to compressed image,
     # e.g. all pixels in that cluster shares the same color as the centroid
-    m = shape( X )[0]
-    X_recovered = zeros( shape(X) )
+    m = shape(X)[0]
+    X_recovered = zeros(shape(X))
 
-    for i in range( 0, m ):
-        k 				= int(idx[i]) - 1
-        X_recovered[i] 	= centroids[k]
+    for i in range(m):
+        k = int(idx[i]) - 1
+        X_recovered[i] = centroids[k]
 
-    X_recovered = X_recovered.reshape( img_size[0], img_size[1], 3 )
-    axes 	= pyplot.gca()
-    figure 	= pyplot.gcf()
-    axes.imshow( X_recovered )
-    pyplot.show( block=True )
+    X_recovered = X_recovered.reshape(img_size[0], img_size[1], 3)
+    pyplot.imshow(X_recovered)
+    pyplot.show(block=True)
 
 
 def main():
     set_printoptions(precision=6, linewidth=200)
-    #part1_2()
-    #part1_4()
+    part1_4()
 
 
 if __name__ == '__main__':
--- ex7/tests/test_k-means.py
+++ ex7/tests/test_k-means.py
@@ -8,19 +8,19 @@ from ex7.kmeans import *
 class TestKMeans(numpy.testing.TestCase):
 
     def setUp(self):
-        mat = scipy.io.loadmat( "../ex7data2.mat" )
+        mat = scipy.io.loadmat("../ex7data2.mat" )
         self.X = mat['X']
         self.K = 3
 
     def test_find_closest_centroids(self):
         initial_centroids = array([[3, 3], [6, 2], [8, 5]])
-        idx = findClosestCentroids(self.X, initial_centroids)
+        idx = find_closest_centroids(self.X, initial_centroids)
         ans = array([[1.], [3.], [2.]])
         numpy.testing.assert_array_equal(idx[0: 3], ans)
 
     def test_compute_centroids(self):
         initial_centroids = array([[3, 3], [6, 2], [8, 5]])
-        idx = findClosestCentroids(self.X, initial_centroids)
+        idx = find_closest_centroids(self.X, initial_centroids)
         centroids = compute_centroids(self.X, idx, self.K)
         centroids = around(centroids, 6)
         ans = array([[2.428301, 3.157924],
@@ -32,3 +32,8 @@ class TestKMeans(numpy.testing.TestCase):
         centroids = kmeans_init_centroids(self.X, self.K)
         for el in centroids:
             numpy.testing.assert_equal(el in self.X, True)
+
+    def test_run_kmeans(self):
+        max_iters = 10
+        centroids = array([[3, 3], [6, 2], [8, 5]])
+        run_kmeans(self.X, centroids, max_iters, plot=True)
